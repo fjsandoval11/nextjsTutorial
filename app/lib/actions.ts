9bcc38b1fb3736613a2cb2e927c77801d7b2a2c8
Sandoval <fsandoval@rocelec.com>
2024-03-25 09:05:44 -0400

    invoice page search and edit + error sitiutions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,15 @@ const FormSchema = z.object({
 const amountInCents = amount * 100
 const date = new Date().toISOString().split('T')[0]
     
-await sql`
+try {await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+} catch (error) {
+  return {
+    message: 'Database Error: Failed to create invoice'
+  }
+}
 
 revalidatePath('/dashboard/invoice')
 redirect('/dashboard/invoice')
@@ -49,17 +54,34 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try{
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;   
+  } catch(error) {
+    return {
+      message: 'Database Error: failed to update invoice'
+    }
+  }
  
   revalidatePath('/dashboard/invoice');
   redirect('/dashboard/invoice');
 }
 
 export async function deleteInvoice(id: string) {
+  
+
+  try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoice');
+    return{
+      message: 'Deleted Invoice'
+    }
+  } catch(error){
+    return{
+      message: 'Database Error: failed to delete invoice'
+    }
+  }
   }
